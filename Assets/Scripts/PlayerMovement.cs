6f5ca000f96c2c2c2f2e41262b7524086c400188
nikolaoravec <oravec.nikola@gmail.com>
2023-06-03 19:31:34 +0200

    Add base animation and base collusion

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(-6, 6, 6);
+        }
+        else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(6,6,-6);
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+}
