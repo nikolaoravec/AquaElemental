86e8e06657449d5366c0526fa7777d972073ae43
nikolaoravec <oravec.nikola@gmail.com>
2023-06-03 20:39:07 +0200

    Remove movement, just leave jump and improve timing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,38 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool isGrounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-
-        if(horizontalInput > 0.01f)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            transform.localScale = new Vector3(-6, 6, 6);
-        }
-        else if(horizontalInput < -0.01f)
-        {
-            transform.localScale = new Vector3(6,6,-6);
-        }
-        
-        if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
+        }
+
+        anim.SetBool("jump", !isGrounded);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
